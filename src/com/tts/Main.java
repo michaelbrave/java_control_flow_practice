6a8c94870f6262f8bd034734590fe90951d19741
Michael Brave <michaelbrave@gmail.com>
2021-01-25 16:38:38 -0600

    question 3

--- src/com/tts/Main.java
+++ src/com/tts/Main.java
@@ -21,20 +21,30 @@ public class Main {
             System.out.print("cool lets continue\n");
             System.out.print("1/6 Do you have a red car? (yes, no):\n");
             String car = scanner.next();
-            int carNum = 0;
-            if (car.equals("yes")) {
-                carNum = 10;
-            }
-            else {
-                carNum = 7;
-            }
+                int carNum = 0;
+                if (car.equals("yes")) {
+                    carNum = 10;
+                }
+                else {
+                    carNum = 7;
+                }
             System.out.print("2/6 What is their lucky number?\n");
             String lucky = scanner.next();
-            int luckyNum = lucky.charAt(0);
+                int luckyNum = lucky.charAt(0);
+                if (lucky.charAt(1) >= 1) {
+                    luckyNum += 10;
+                    luckyNum += lucky.charAt(1);
+                }
             System.out.print("3/6 Do they have a favorite quarterback?  If so what is their jersey number?\n");
             String jersey = scanner.next();
+                int jerseyNum = jersey.charAt(0);
+                jerseyNum += 10;
+                if (jersey.charAt(1) >= 1) {
+                    jerseyNum += jersey.charAt(1);
+                }
             System.out.print("4/6 What is two-digit model year of their car?\n");
             String year = scanner.next();
+                
             System.out.print("5/6 What is the first name of the their favorite actor or actress?\n");
             String actor = scanner.next();
             System.out.print("6/6 Enter a random number between 1 and 50.\n");
@@ -45,6 +55,9 @@ public class Main {
             System.out.print("you've chosen to quit, exiting now, please return later to complete the survey");
             return;
         }
+
+    }
+    public  static void question1() {
 
     }
 }
