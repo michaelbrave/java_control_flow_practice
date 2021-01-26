25b46feb490a3141da930728aec9de255d8acb32
Michael Brave <michaelbrave@gmail.com>
2021-01-25 21:51:29 -0600

    all but actor should work

--- src/com/tts/Main.java
+++ src/com/tts/Main.java
@@ -59,44 +59,32 @@ public class Main {
     public static int question2(){
         Scanner scanner = new Scanner(System.in);
         System.out.print("2/6 What is their lucky number?\n");
-        String lucky = scanner.next(); // error string index out of range
-        int luckyNum = (int)lucky.charAt(0);
-        if (lucky.charAt(1) >= 1) {
-            luckyNum += 10;
-            luckyNum += lucky.charAt(1);
-        }
+        int luckyNum = scanner.nextInt();
         return luckyNum;
     }
     public static int question3(){
         Scanner scanner = new Scanner(System.in);
         System.out.print("3/6 Do they have a favorite quarterback?  If so what is their jersey number?\n");
-        String jersey = scanner.next();
-        int jerseyNum = jersey.charAt(0);
-        jerseyNum += 10;
-        if (jersey.charAt(1) >= 1) {
-            jerseyNum += jersey.charAt(1);
-        }
+        int jerseyNum = scanner.nextInt();
         return jerseyNum;
     }
     public static int question4(){
         Scanner scanner = new Scanner(System.in);
         System.out.print("4/6 What is two-digit model year of their car?\n");
-        String year = scanner.next();
-        int modelNum = 0;
+        int modelNum = scanner.nextInt();
         return modelNum;
     }
     public static int question5(){
         Scanner scanner = new Scanner(System.in);
         System.out.print("5/6 What is the first name of the their favorite actor or actress?\n");
         String actor = scanner.next();
-        int actorNum = 0;
+        int actorNum = actor.charAt(0);
         return actorNum;
     }
     public static int question6(){
         Scanner scanner = new Scanner(System.in);
         System.out.print("6/6 Enter a random number between 1 and 50.\n");
-        String randomly = scanner.next();
-        int randomNum = 0;
+        int randomNum = scanner.nextInt();
         return randomNum;
     }
     public static void numGen(int carNum, int luckyNum, int jerseyNum, int modelNum, int actorNum, int randomNum){
